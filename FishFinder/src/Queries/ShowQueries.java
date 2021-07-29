952b665c7dacc1783f2057578ecc24584fe8f2bd
Trish Nguyen <74442131+trishnguyen2001@users.noreply.github.com>
2021-07-28 22:29:12 -0700

    File Reorganization: Added Tour and Show Queries

--- FishFinder/src/Queries/ShowQueries.java
+++ FishFinder/src/Queries/ShowQueries.java
@@ -1,5 +1,63 @@
 package Queries;
 
-public class ShowQueries {
+import java.sql.*;
+
+public class ShowQueries{
+	private Connection con;
+	private Statement stmt;
+
+	public ShowQueries() {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "system", "Oracle.com0709");
+			stmt = con.createStatement();
+
+		} catch (Exception e) {
+			System.out.println("ERROR: " + e.getMessage());
+		}	
+	}
+
+	public String showNameQuery(String showName) {
+		System.out.println("EXECUTING: showNameQuery");
+
+		if(showName == null || showName.equals("")) {
+			System.out.println("EXECUTING: empty showNameQuery");
+			return "SELECT L.liveshowID FROM LiveShow L";	
+		}
+		showName = showName.toUpperCase();
+		return "SELECT L.liveshowID FROM LiveShow L WHERE L.sName LIKE '%" + showName + "%'";	
+	}
+
+	public String timeQuery(String time) {
+		System.out.println("EXECUTING: tourNameQuery");
+
+		if(time == null || time.equals("")) {
+			System.out.println("EXECUTING: empty timeQuery");
+			return "SELECT L.liveshowID FROM LiveShow L";	
+		}
+
+		return "SELECT L.liveshowID FROM LiveShow L WHERE L.time LIKE '%" + time + "%'";	
+	}
+
+	public String showAnimalQuery(String animal) {
+		System.out.println("EXECUTING: tourNameQuery");
+
+		if(animal == null || animal.equals("")) {
+			System.out.println("EXECUTING: empty showAnimalQuery");
+			return "SELECT L.liveshowID FROM LiveShow L";	
+		}
+		animal = animal.toUpperCase();
+		return "SELECT L.liveshowID FROM LiveShow L, AnimalPerformsInShow APIS WHERE APIS.general_name LIKE '%" + animal + "%' AND L.liveshowID = APIS.liveshowID";	
+	}
+
+
+	public ResultSet intersection(String nq, String tq, String aq) throws SQLException {
+		return stmt.executeQuery(	"SELECT L.sName, L.time, APIS.general_name " + 
+				"FROM LiveShow L, AnimalPerformsInShow APIS " +
+				"WHERE L.liveshowID IN ((" + nq + ") INTERSECT (" + tq + ") INTERSECT (" + aq + ")) " +  
+				"AND L.liveshowID = APIS.liveshowID " +
+				"ORDER BY L.liveshowID" 
+				);
+	}
 
 }
--- FishFinder/src/Queries/TourQueries.java
+++ FishFinder/src/Queries/TourQueries.java
@@ -1,10 +1,6 @@
 package Queries;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class TourQueries {
 	
@@ -14,7 +10,7 @@ public class TourQueries {
 	public TourQueries() {
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
-			con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "system", "newOracle");
+			con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "system", "Oracle.com0709");
 			stmt = con.createStatement();
 
 		} catch (Exception e) {
@@ -26,21 +22,41 @@ public class TourQueries {
 		System.out.println("EXECUTING: tourNameQuery");
 		
 		if(tourName == null || tourName.equals("")) {
-			System.out.println("EXECUTING: empty genNameQuery");
+			System.out.println("EXECUTING: empty tourNameQuery");
 			return "SELECT T.tourID FROM Tour T";	
 		}
-		
-		tourName = tourName.replaceAll(" ", "");
-		
-		
+		tourName = tourName.toUpperCase();
 		return "SELECT T.tourID FROM Tour T WHERE T.tName LIKE '%" + tourName + "%'";	
 	}
 	
+	public String timeQuery(String time) {
+		System.out.println("EXECUTING: tourNameQuery");
+		
+		if(time == null || time.equals("")) {
+			System.out.println("EXECUTING: empty timeQuery");
+			return "SELECT T.tourID FROM Tour T";	
+		}
+		
+		return "SELECT T.tourID FROM Tour T WHERE T.time LIKE '%" + time + "%'";	
+	}
 	
-	public ResultSet intersection(String nq) throws SQLException {
+	public String zoneQuery(String zone) {
+		System.out.println("EXECUTING: tourNameQuery");
+		
+		if(zone == null || zone.equals("")) {
+			System.out.println("EXECUTING: empty zoneQuery");
+			return "SELECT T.tourID FROM Tour T";	
+		}
+		
+		zone = zone.toUpperCase();
+		return "SELECT T.tourID FROM Tour T, ZoneTours ZT WHERE ZT.zoneID = '" + zone + "' AND T.tourID = ZT.tourID";	
+	}
+	
+	
+	public ResultSet intersection(String nq, String tq, String zq) throws SQLException {
 		return stmt.executeQuery(	"SELECT T.tName, T.time, ZT.zoneID " + 
 									"FROM Tour T, ZoneTours ZT " +
-									"WHERE T.tourID IN (" + nq + ") " +  
+									"WHERE T.tourID IN ((" + nq + ") INTERSECT (" + tq + ") INTERSECT (" + zq + ")) " +  
 									"AND T.tourID = ZT.tourID " +
 									"ORDER BY T.tourID" 
 								);
